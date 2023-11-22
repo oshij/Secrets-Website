c5830d577bd2ec4c7c208694386336b02b98e892
oshij <oshijtrivedi@gmail.com>
2023-11-23 02:22:50 +0530

    Authentication Level 2 - Encryption using crypto

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ const db = new pg.Client({
     port : process.env.DB_PORT,
 
 })
-var algorithm = 'aes256';
-var key = 'password';
+
 
 db.connect();
 console.log("Database Connected !!!!");
@@ -39,7 +38,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",async (req,res) => {
     const name = req.body.username;
     const password = req.body.password;
-    var cipher = crypto.createCipher(algorithm, key);  
+    var cipher = crypto.createCipher(process.env.ALGORITHM, process.env.KEY);  
     var encrypted = cipher.update(password, 'utf8', 'hex') + cipher.final('hex');
     try{
         const result = await db.query("INSERT INTO users(username,password) VALUES ($1,$2)",[name,encrypted]);
@@ -56,7 +55,7 @@ app.post("/login", async(req,res)=>{
     try{
         const result = await db.query("SELECT * FROM users WHERE username = $1",[username]);
         if(result.rows.length !== 0){
-            var decipher = crypto.createDecipher(algorithm, key);
+            var decipher = crypto.createDecipher(process.env.ALGORITHM, process.env.KEY);
             var decrypted = decipher.update(result.rows[0].password, 'hex', 'utf8') + decipher.final('utf8');
             if( decrypted === password){
                 res.render("secrets")
