c0a3920b0460d55bd35935235f97af75abeda5bb
oshij <oshijtrivedi@gmail.com>
2023-11-22 23:31:49 +0530

    Authentication Level 1 complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -8,13 +9,54 @@ app.set('view engine','ejs')
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended : true}));
 
+const db = new pg.Client({
+    user : "postgres",
+    database : "world",
+    host : "localhost",
+    password : "A9wksB@1",
+    port : 5432,
 
-app.get("/",(req,res) => { 
-    res.render("home")
 })
 
+db.connect();
 
+app.get("/",(req,res) => { 
+    res.render("home");
+})
 
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+app.get("/register",(req,res)=>{
+    res.render("register");
+})
+
+app.post("/register",async (req,res) => {
+    const name = req.body.username;
+    const password = req.body.password;
+    try{
+        const result = await db.query("INSERT INTO users(username,password) VALUES ($1,$2)",[name,password]);
+        res.render("secrets")
+    }
+    catch(err){
+        console.log(err)
+    }
+    
+})
+app.post("/login", async(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    try{
+        const result = await db.query("SELECT * FROM users WHERE username = $1",[username]);
+        if(result.rows.length !== 0){
+            if(result.rows[0].password === password){
+                res.render("secrets")
+            }  
+        }
+    }catch(err){
+        console.log(err);
+    }
+})
 
 app.listen(port,()=>{
     console.log(`Server is listening on port : ${port}`)
